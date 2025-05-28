435a4bbc9f0a7a9dcb9713ffd80bccefbceee74b
LauraMartinez99 <lauramartinez12007@gmail.com>
2025-05-27 21:39:28 -0500

    Efecto tarjetas certificaciones

--- src/components/CertificationCard.tsx
+++ src/components/CertificationCard.tsx
@@ -10,7 +10,8 @@ interface CertificationCardProps {
 
 const CertificationCard: React.FC<CertificationCardProps> = ({ title, imageUrl }) => {
   return (
-    <div className="bg-white rounded-lg shadow-lg overflow-hidden transition-all duration-300 hover:shadow-xl hover:-translate-y-1 flex flex-col h-[300px]">
+    <div className="bg-white rounded-lg shadow-lg overflow-hidden transition-all duration-300 hover:shadow-xl hover:-translate-y-1 flex flex-col h-[300px] group relative">
+      <div className="absolute inset-0 rounded-lg transition-all duration-300 group-hover:shadow-[0_0_25px_rgba(0,149,255,0.5)] group-hover:border-[1px] group-hover:border-[#0095ff] pointer-events-none z-10"></div>
       <div className="relative flex-grow">
         <Image
           src={imageUrl}
